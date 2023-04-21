5156d2354a992a6bc534701fc264dafea5b10fac
colin <colin.williams91@gmail.com>
2023-04-21 14:54:08 -0500

    event emitters for categories and panel views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,14 +19,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component'; /* <-- `$ng g c` CLI imports and declares for component use/routing */
+import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
+import { FiltersComponent } from './pages/home/components/filters/filters.component'; /* <-- `$ng g c` CLI imports and declares for component use/routing */
 
 @NgModule({
   declarations: [ /* <-- allows components to be routed and used */
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    ProductsHeaderComponent
+    ProductsHeaderComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
