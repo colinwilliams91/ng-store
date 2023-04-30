d6065b8b527238ac11137f0f065ebeebb307144c
colin <colin.williams91@gmail.com>
2023-04-29 22:33:20 -0500

    add cart to header, get set, dynamic cart quantity

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  private _cart: Cart = { items: [] };
+  itemsQuantity = 0;
+
+  @Input()
+  get cart(): Cart {
+    return this._cart;
+  }
+
+  set cart(cart: Cart) {
+    this._cart = cart;
+
+    this.itemsQuantity = cart.items
+      .map(item => item.quantity)
+      .reduce((acc, cur) => acc + cur, 0)
+  }
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
