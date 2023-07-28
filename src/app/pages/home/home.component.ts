052f0b179c6ac459c464484778e0790fa3a7ed98
colin <colin.williams.dev@gmail.com>
2023-07-28 15:15:35 -0500

    feat: fakeStoreAPI hit and render

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product.model'; // <-- model for onAddToCart
 import { CartService } from 'src/app/services/cart.service';
+import { StoreService } from 'src/app/services/store.service';
 
 const ROWS_HEIGHT: { [id:number]: number } = { 1: 400, 3: 335, 4: 350 };
 
@@ -8,14 +10,26 @@ const ROWS_HEIGHT: { [id:number]: number } = { 1: 400, 3: 335, 4: 350 };
   selector: 'app-home', // <-- css selector for how this component will be used in html
   templateUrl: './home.component.html', // rel-path to this component's html file (like JSX)
 })
-export class HomeComponent implements OnInit { // <-- typescript Class that defines component behavior
+export class HomeComponent implements OnInit, OnDestroy { // <-- typescript Class that defines component behavior
   cols = 3;
   rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
+  products: Array<Product> | undefined;
+  sort = 'desc';
+  count = '12'; // TODO: <-- limit?
+  productSubscription: Subscription | undefined;
 
-  constructor(private cartService: CartService) { } // <-- inject Service to add to cart
+  constructor(private cartService: CartService, private storeService: StoreService) { } // <-- inject Service to add to cart
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productSubscription = this.storeService.getAllProducts(this.count, this.sort)
+      .subscribe((_products) => {
+        this.products = _products;
+      });
   }
 
   onColumnsCountChange(colsNum: number): void {
@@ -35,6 +49,12 @@ export class HomeComponent implements OnInit { // <-- typescript Class that defi
       quantity: 1,
       id: product.id
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.productSubscription) {
+      this.productSubscription.unsubscribe();
+    }
   }
 
 }
